8f617a85922a93b0a829f31dbc2ff6f83caa35eb
Zhanxi Ni <ni.zhanxi@gmail.com>
2019-06-04 23:53:47 -0700

    created express server and named it app

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// Middleware
+
+// Routes
+
+// Start Server
+
+app.listen(3000, () => {
+  console.log('Gif Search listening on port localhost:3000!');
+});
